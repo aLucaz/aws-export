264e522c72e82f13bcd20ce73d29bd1115556467
aLucaz <arturo.lucas.pe@gmail.com>
2023-01-30 12:50:32 -0500

    Adding header line

--- src/utils/excel.builder.js
+++ src/utils/excel.builder.js
@@ -2,7 +2,7 @@ import xlsx from 'xlsx'
 
 export class ExcelBuilder {
   static jsonToExcelBuffer(data) {
-    let sheet = xlsx.utils.json_to_sheet(data, {skipHeader: true});
+    let sheet = xlsx.utils.json_to_sheet(data, {skipHeader: false});
     let book = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(book, sheet);
     return xlsx.write(book, {type:'buffer', bookType:'xlsx'});
